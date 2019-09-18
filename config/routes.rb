7cecc42025cd3e9ec734aca084d5ae8637b6f321
AlexIv <alx.ekb@gmail.com>
2019-09-18 23:50:04 +0500

    post

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :posts
+  devise_for :users do
+    get '/users/sign_out', to: 'devise/sessions#destroy', :method => :delete
+  end
+  root 'posts#index'
 end
